00d9d5b1ab66eeb256a4041df36d6248bcc5bc6b
Raviexthegod <xaviercoffey0@gmail.com>
2025-07-01 14:22:38 -0400

    FMT

--- configuration.nix
+++ configuration.nix
@@ -176,10 +176,8 @@
       }).fd];
     };
   };
-  
 
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     vscode
     wget
