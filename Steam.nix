fcb423b0d4d4899ad3a54a2d4fed9872fdb23898
Raviexthegod <xaviercoffey0@gmail.com>
2025-07-02 10:52:57 -0400

    disable gamemode

--- Steam.nix
+++ Steam.nix
@@ -14,32 +14,6 @@
     gamescopeSession.enable = true;
   };
 
-  programs.gamemode = {
-    enable = true;
-    enableRenice = true;
-    settings = {
-      general = {
-        renice = 10;
-      };
-
-      # Warning: GPU optimisations have the potential to damage hardware
-      gpu = {
-        apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 0;
-        amd_performance_level = "high";
-      };
-
-      cpu = {
-        park_cores = "yes";
-        pin_cores = "yes";
-      };
-
-      custom = {
-        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
-      };
-    };
-  };
   programs.gamescope = {
     enable = true;
     capSysNice = true;
