978adf886b1dcf43d3a1dec8b0186ff602d30fee
Raviexthegod <xaviercoffey0@gmail.com>
2025-07-06 13:03:54 -0400

    remove Gamescope session

--- Steam.nix
+++ Steam.nix
@@ -16,16 +16,5 @@
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
     remotePlay.openFirewall = true;
-    gamescopeSession.enable = true;
   };
-
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;
-  };
-  hardware.xone.enable = true;
-  services.getty.autologinUser = "raviex";
-  environment.loginShellInit = ''
-    [[ "(tty)" = "/dev/tty1" ]] && ./gs.sh
-  '';
 }
