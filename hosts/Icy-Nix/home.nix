a8127acc27f93756f3f7f871b29e6144e737f0b1
Raviexthegod <xaviercoffey0@gmail.com>
2025-09-15 05:26:17 -0400

    remove zsh-autoenv

--- hosts/Icy-Nix/home.nix
+++ hosts/Icy-Nix/home.nix
@@ -89,7 +89,6 @@
         }
       ];
       initContent = ''source ~/.p10k.zsh'';
-      zsh-autoenv.enable = true;
       initExtra = ''
         source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
       '';
